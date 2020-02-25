3dd472f05397fc39fd221c3e4224e493b6e4e714
izzy-asplet <isabelasplet@gmail.com>
2020-02-25 15:00:40 +1300

    afternoon

--- client/api/index.js
+++ client/api/index.js
@@ -1,7 +1,8 @@
 import request from 'superagent'
 
 export function getBennes() {
-  return request.get('/v1/bennes')
+  return request
+  .get('/v1/bennes')
   .then(res => {
     return res.body
   })
@@ -10,6 +11,13 @@ export function getBennes() {
 export function saveBenne(benne) {
   return request
     .post('/v1/bennes')
+    .insert({
+      cafe_name: benne.cafe_name,
+      image: benne.image,
+      variation: benne.variation,
+      price: benne.price,
+      rating: benne.rating
+    })
     .send(benne)
     .then(res => res.body)
 }
